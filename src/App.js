0deb2a296446a19f55924df8fd9b8b569d8de9f9
Luís Eduardo Marques Dias <leduardomdias@gmail.com>
2024-10-17 13:07:42 -0300

    ...

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+class Equipe extends Component {
+  render() {
+    return (
+      <Sobre nome = {this.props.nome} cargo = {this.props.cargo} idade = {this.props.idade} />
+    );
+  }
+}
+
+class Sobre extends Component {
+  render(){
+    return(
+      <div>
+        <h2> Olá sou o(a) {this.props.nome} </h2>
+        <h2> Cargo: {this.props.cargo} </h2>
+        <h2> Idade: {this.props.idade} </h2>
+        <hr/>
+    </div> 
+    );
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Conheça nossa Equipe!</h1>
+      <Equipe nome="Luis" cargo = "Dev Front" idade = "10"/>
+      <Equipe nome="Rafa" cargo = "Design" idade = "13"/>
     </div>
   );
 }
